b4a800ac22767accd0004faf9a179941174c6d07
Jake-Silveira <jacob1silveira@gmail.com>
2025-03-12 17:53:46 -0500

    manipulated modal with .js for socials & gallery

--- scripts.js
+++ scripts.js
@@ -1,15 +1,37 @@
 // Get the modal
 var modal = document.getElementById("menuModal");
+var modalContent = document.getElementById("modalContent");
+var menuList = document.getElementById("menuList");
+var socialsList = document.getElementById("socialsList");
 
 // Get the button that opens the modal
-var btn = document.getElementById("menuBtn");
+var menuBtn = document.getElementById("menuBtn");
+var socialsBtn = document.getElementById("socialsBtn");
+var galleryBtn = document.getElementById("galleryBtn");
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+menuBtn.onclick = function() {
   modal.style.display = "block";
+  modalContent.style.margin = "0 0";
+  modalContent.style.width = "200px";
+  modalContent.style.height = "100%";
+  modalContent.style.borderRadius = "0px";
+  modalContent.style.padding = "4px";
+  socialsList.style.display = "none";
+  menuList.style.display = "flex";
+}
+
+socialsBtn.onclick = function() {
+  wideModal();
+  socialsList.style.display = "flex";
+}
+
+galleryBtn.onclick = function() {
+  wideModal();
+  socialsList.style.display = "none";
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -22,4 +44,23 @@ window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
+}
+
+window.ontouchstart = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+const wideModal = function(){
+  modal.style.display = "flex";
+  modal.style.alignItems = "center";
+  modal.style.justifyContent = "center";
+  modalContent.style.width = "80%";
+  modalContent.style.minWidth = "300px";
+  modalContent.style.height = "80%";
+  modalContent.style.minHeight = "300px";
+  modalContent.style.borderRadius = "20px";
+  modalContent.style.padding = "20px";
+  menuList.style.display = "none";
 }
