eb34140b756f75d33e11f85bb251bfe5a4d16acf
Jake-Silveira <jacob1silveira@gmail.com>
2025-07-28 18:16:29 -0500

    fixed desc space issue

--- backend/public/scripts.js
+++ backend/public/scripts.js
@@ -327,10 +327,12 @@ function sanitizeInput(str) {
 };
 
 function stripEmojis(str) {
+  // Emoji regex from Unicode v13+ compatible sources
   return str.replace(
-    /([\u203C-\u3299]|[\uD83C-\uDBFF\uDC00-\uDFFF])+/g,
+    /([\u231A-\u231B]|\u23E9|\u23EA|\u23EB|\u23EC|\u23F0|\u23F3|\u25FD|\u25FE|\u2614|\u2615|\u2648-\u2653|\u267F|\u2693|\u26A1|\u26AA|\u26AB|\u26BD|\u26BE|\u26C4|\u26C5|\u26CE|\u26D4|\u26EA|\u26F2|\u26F3|\u26F5|\u26FA|\u26FD|\u2702|\u2705|\u2708-\u2709|\u270A-\u270B|\u2728|\u2733|\u2734|\u2744|\u2747|\u2753-\u2755|\u2757|\u2764|\u2795-\u2797|\u27B0|\u27BF|\u2B50|\u2B55|\u1F004|\u1F0CF|\u1F170-\u1F171|\u1F17E-\u1F17F|\u1F18E|\u1F191-\u1F19A|\u1F1E6-\u1F1FF|\u1F201-\u1F202|\u1F21A|\u1F22F|\u1F232-\u1F23A|\u1F250-\u1F251|\u1F300-\u1F320|\u1F330-\u1F335|\u1F337-\u1F37C|\u1F380-\u1F393|\u1F3A0-\u1F3C4|\u1F3C6-\u1F3CA|\u1F3E0-\u1F3F0|\u1F400-\u1F43E|\u1F440|\u1F442-\u1F4F7|\u1F4F9-\u1F4FC|\u1F500-\u1F53D|\u1F550-\u1F567|\u1F5FB-\u1F640|\u1F645-\u1F64F|\u1F680-\u1F6C5|\u1F6CC|\u1F6D0|\u1F6D1-\u1F6D2|\u1F6EB-\u1F6EC|\u1F6F0|\u1F6F3|\u1F910-\u1F93E|\u1F940-\u1F970|\u1F973-\u1F976|\u1F97A|\u1F97C-\u1F9A2|\u1F9B0-\u1F9B9|\u1F9C0-\u1F9C2|\u1F9D0-\u1F9FF|\u1FA70-\u1FA74|\u1FA78-\u1FA7A|\u1FA80-\u1FA82|\u1FA90-\u1FA95|\u1FAD0-\u1FAD9)/gu,
     ''
-)};
+  );
+}
 
   function validateDates() {
     const fromInput = document.getElementById('inquiryAvailabilityFrom');
@@ -368,7 +370,7 @@ function stripEmojis(str) {
     return true;
   };
 function cleanInput(value) {
-  return sanitizeInput(stripEmojis(value.trim()));
+  return sanitizeInput(stripEmojis(value));
 };
 function enableSubmit(){
   document.getElementById('inquirySubmitBtn').disabled = false;
