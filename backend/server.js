cf7d10af60fdacee870e02f8702dbe43339baf45
Jake-Silveira <jacob1silveira@gmail.com>
2025-08-06 13:07:18 -0500

    fixed cookies in fetch requests

--- backend/server.js
+++ backend/server.js
@@ -46,7 +46,10 @@ const upload = multer({
 
 
 // === Middleware ===
-app.use(cors());
+app.use(cors({
+  origin: 'https://ravensnest.ink', 
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('trust proxy', true); // Allows req.ip to be accurate behind a reverse proxy
